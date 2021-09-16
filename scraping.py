7fb46bdbb467020a861ce0c7762b2005109e8f3c
aeronut343 <halex211@umn.edu>
2021-09-16 17:20:05 -0500

    challenge partial

--- scraping.py
+++ scraping.py
@@ -4,9 +4,24 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    # Run all scraping functions and store results in dictionary
+    news_title, news_paragraph = mars_news(browser)
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
     # Visit the mars nasa news site
@@ -62,11 +77,12 @@ def mars_facts():
     except BaseException:
         return None
     
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
-    return df.to_html()
+    df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    df.columns=["Description","Mars","Earth"]
+    df.set_index("Description",inplace=True)
+    return df.to_html(classes="table table-striped")
 
-browser.quit()
+if __name__ == "__main__":
 
-
-
+    # If running as script, print scraped data
+    print(scrape_all())
